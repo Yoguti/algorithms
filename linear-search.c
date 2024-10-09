b6c074e1070801586dff54264ab44e411fcb3400
KlausSchneider <165155261+Yoguti@users.noreply.github.com>
2024-10-08 21:50:52 -0300

    Update linear-search.c

--- linear-search.c
+++ linear-search.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 // Function prototypes
-int linearSearch(int haystack[], int needle, int heylenght);
+int linearSearch(int haystack[], int needle, int haylenght);
 
 int main() {
 
-    int heylenght = 10;
+    int haylenght = 10;
     int needle = 100;
-    int heystack[heylenght];
-    for (int i = 0; i < heylenght; i++){
+    int heystack[haylenght];
+    for (int i = 0; i < haylenght; i++){
         heystack[i] = i+1;
     }
 
@@ -17,7 +17,7 @@ int main() {
     }
     printf("\n");
     printf("linear search result: \n");
-    int save = linearSearch(heystack, needle, heylenght);
+    int save = linearSearch(heystack, needle, haylenght);
     if( save == 0){
         printf("found\n");
     } else {
@@ -26,8 +26,8 @@ int main() {
     return 0;
 }
 
-int linearSearch(int haystack[], int needle, int heylenght) {
-    for (int i = 0; i < heylenght; i++) {
+int linearSearch(int haystack[], int needle, int haylenght) {
+    for (int i = 0; i < haylenght; i++) {
         if(haystack[i] == needle) {
             return 0;
         }
